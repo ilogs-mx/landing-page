eace433f6c46e908b6ce0430056c22e232dc11d7
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2025-05-30 14:41:33 -0500

    Add BenefitCard component and benefits data structure

---
+++ src/utils/constains.ts
@@ -0,0 +1,39 @@
+import Qr from "../assets/svg/qr.svg";
+import Notification from "../assets/svg/notification.svg";
+import Calendar from "../assets/svg/calendar.svg";
+import Setting from "../assets/svg/setting.svg";
+import Security from "../assets/svg/security.svg";
+import Family from "../assets/svg/family.svg";
+
+export const benefits = [
+    {
+        title: "Control por QR",
+        subtitle: "Control por códigos QR únicos y autorizaciones verificadas.",
+        icon: Qr,
+    },
+    {
+        title: "Notificaciones en tiempo real",
+        subtitle: "Notificaciones instantáneas ante cualquier movimiento.",
+        icon: Notification,
+    },
+    {
+        title: "Ahorro de tiempo y reducción de errores",
+        subtitle: "Optimiza el proceso de salida diaria.",
+        icon: Calendar,
+    },
+    {
+        title: "Fácil implementación",
+        subtitle: "Integración sin complicaciones ni pérdida de tiempo.",
+        icon: Setting,
+    },
+    {
+        title: "Mayor seguridad",
+        subtitle: "Refuerza la seguridad escolar con procesos de salida totalmente verificados.",
+        icon: Security,
+    },
+    {
+        title: "Colaboración con padres",
+        subtitle: "Facilita la comunicación y cooperación entre padres y personal escolar.",
+        icon: Family,
+    },
+];
