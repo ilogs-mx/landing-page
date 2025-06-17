68e0b03242e156617b633b52e8a1ad1b684206df
Tania Cristel Perez Gonzalez <taniagonzalez1230@gmail.com>
2025-06-17 16:02:31 -0500

    Add blog FAQs

---
+++ src/utils/faqs.ts
@@ -0,0 +1,54 @@
+
+
+export const faqs = [
+    {
+    question: "¿Cómo funciona la app?",
+    answer: "iLogs es una aplicación móvil diseñada  para garantizar que los estudiantes únicamente sean entregados a personas previamente autorizadas, mediante un sistema de identificación por código QR.",
+    number : "1",
+  },
+  {
+    question: "¿Quiénes deben descargar la aplicación?",
+    answer: "La aplicación debe de ser descargada por tutores y al personal autorizado de las instituciones educativas encargadas de realizar la entrega de los alumnos.",
+    number : "2",
+  },
+  {
+    question: "¿Cómo opera iLogs desde el perfil del padre o tutor?",
+    answer: "El padre o tutor debe seleccionar al alumno que será recogido desde la aplicación, lo cual genera un código QR único. Este código debe ser presentado al personal escolar al momento de la entrega.",
+    number : "3",
+  },
+  {
+    question: "¿Qué sucede si el padre o tutor no puede acudir a recoger al alumno?",
+    answer: "Puede autorizar a un familiar o amigo previamente registrado en la aplicación, generando para dicha persona un código QR exclusivo que permitirá la entrega segura del alumno.",
+    number : "4",
+  },
+  {
+    question: "¿Qué tipo de información se muestra al personal escolar al escanear un código QR?",
+    answer: " El personal autorizado visualizará los datos esenciales del alumno, como su nombre, grupo y salón de clases, permitiendo así una localización rápida y una entrega segura.",
+    number : "5",
+  },
+  {
+    question: "¿Es necesario contar con conexión a internet para utilizar iLogs?",
+    answer: "No. Una de las ventajas de iLogs es que funciona sin necesidad de conexión a internet, lo que permite su uso eficiente incluso en entornos con conectividad limitada.",
+    number : "6",
+  },
+  {
+    question: "¿En qué dispositivos se encuentra disponible iLogs?",
+    answer: "La aplicación está disponible de forma gratuita para su descarga en dispositivos móviles con sistema operativo iOS (App Store) y Android (Google Play Store",
+    number : "7",
+  },
+  {
+    question: "¿Se notifica al tutor cuando el alumno ha sido entregado?",
+    answer: " Sí. Una vez que el alumno ha sido entregado, el tutor recibirá una notificación inmediata a través de la aplicación, indicando la hora exacta y la persona que realizó la recolección.",
+    number : "8",
+  },
+  {
+    question: "¿Es seguro compartir el código QR con otra persona?",
+    answer: "Sí, siempre y cuando la persona se encuentre registrada y autorizada previamente por el tutor dentro de la plataforma.",
+    number : "9",
+  },
+  {
+    question: "¿iLogs cuenta con medidas de seguridad para proteger la información?",
+    answer: " Sí. Toda la información se transmite de forma cifrada y los accesos están protegidos con credenciales. Además, el historial de accesos está disponible únicamente para usuarios autorizados.",
+    number : "10",
+  },
+];
